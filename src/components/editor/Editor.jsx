48ea0e589d5c0dbca93ef2ef3882a7363d7b7e22
hansol-FE <dundunki@naver.com>
2022-07-05 20:17:36 +0900

    add editor component

---
+++ src/components/editor/Editor.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import CardEditForm from '../card_edit_form/CardEditForm';
+
+import styles from './editor.module.css';
+
+const Editor = ({cards}) => {
+  return (
+    <section className={styles.editor}>
+      <h1 className={styles.title}>Card Maker</h1>
+      {
+        cards.map(card => (
+          <CardEditForm key={card.id} card={card} />
+        ))
+      }
+    </section>
+  );
+};
+
+export default Editor;
